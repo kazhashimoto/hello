6e4bdf17acbc80f0ba39d99982d555dd6b058598
kazhashimoto <zkar.h62@gmail.com>
2021-07-25 08:38:44 +0900

    Initial commit

---
+++ bin/hello.js
@@ -0,0 +1,19 @@
+const { program } = require("commander");
+const debug = require('debug')('hello');
+
+program
+  .name('hello')
+  .version('0.1.0')
+  .usage('[options]')
+  .showHelpAfterError()
+  .option('-u, --upper', 'convert the message to upper case');
+
+program.parse(process.argv);
+const options = program.opts();
+debug('options', options);
+
+let greeting_msg = 'hello, world!'
+if (options.upper) {
+  greeting_msg = greeting_msg.toUpperCase();
+}
+console.log(greeting_msg);
